7004afff43abe31a5c4f943616f28ed95af7f8c1
Dave Farinelli <d@dfar.io>
2018-06-15 10:59:49 -0400

    Changing home page image to JPG

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -20,7 +20,7 @@ export class HomeComponent implements OnInit {
   featuredMindShare: MindShare;
   featuredOpenPositions: OpenPosition[];
   collectedOpenPositions: CollectedOpenPosition[];
-  heroImageUrl = 'home-page.png';
+  heroImageUrl = 'home-page.jpg';
 
   constructor(
     private route: ActivatedRoute,
