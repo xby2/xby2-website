e68184afcb2400a3aa3f1bf67e69ecb138b9ea40
clouzon <clouzon@elevenwinds.com>
2018-11-09 08:53:23 -0500

    Switch staging to use staging API, switch production to use production API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,28 +3,29 @@ export const environment = {
   apiUrl: 'https://xby2-website-api.azurewebsites.net',
   googleAnalyticsTrackingId: 'UA-114675726-2',
   assetPrefixes: {
-    clientStories: './assets/client-story/',
-    perks: './assets/perks/',
-    companyValues: './assets/values/'
+    clientStories : '',
+    perks : '',
+    companyValues : '',
+    authors: ''
   },
-
   /*
     For local development using the mock API data, set usingLocalData to true.
     This will connect to local JSON files instead of the CMS.
   */
-  usingLocalData: true,
-  baseCmsUrl: '../assets/data/',
+  usingLocalData: false,
+  baseCmsUrl:
+    'https://xby2-website-cms.azurewebsites.net/wp-json/xby2/v1/',
   endpoints: {
-    companyValues: 'company-values.json',
-    frequentlyAskedQuestions: 'frequently-asked-questions.json',
-    perks: 'perks.json',
-    recruitingValues: 'recruiting-values.json',
-    locations: 'locations.json',
-    clientStories: 'client-stories.json',
-    industries: 'industries.json',
-    links: 'links.json',
-    mindShares: 'mind-shares.json',
-    services: 'services.json',
+    companyValues: 'company-values',
+    frequentlyAskedQuestions: 'frequently-asked-questions',
+    perks: 'perks',
+    recruitingValues: 'recruiting-values',
+    locations: 'locations',
+    clientStories: 'client-stories',
+    industries: 'industries',
+    links: 'links',
+    mindShares: 'mind-shares',
+    services: 'services',
     media: 'media.json'
   }
 };
