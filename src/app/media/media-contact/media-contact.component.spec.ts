2e46443e1d46404168913b85a484c28916c49a0b
Dave Farinelli <d@dfar.io>
2019-02-17 15:54:02 -0500

    XBW-131: Content for media page created, styling & contact verif. needed

--- src/app/media/media-contact/media-contact.component.spec.ts
+++ src/app/media/media-contact/media-contact.component.spec.ts
@@ -1,16 +1,33 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MediaContactComponent } from './media-contact.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ContactService } from '../../shared/service/contact.service';
+import { TOASTR_TOKEN } from '../../core/toastr.service';
 
 describe('MediaContactComponent', () => {
   let component: MediaContactComponent;
   let fixture: ComponentFixture<MediaContactComponent>;
 
   beforeEach(async(() => {
+    const mockContactService = {};
+    const mockToastr = {};
+
     TestBed.configureTestingModule({
-      declarations: [ MediaContactComponent ]
+      imports: [ReactiveFormsModule],
+      declarations: [MediaContactComponent],
+      providers: [
+        {
+          provide: ContactService,
+          useValue: mockContactService
+        },
+        {
+          provide: TOASTR_TOKEN,
+          useValue: mockToastr
+        }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
--- src/app/media/media.component.spec.ts
+++ src/app/media/media.component.spec.ts
@@ -1,16 +1,31 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MediaComponent } from './media.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 describe('MediaComponent', () => {
   let component: MediaComponent;
   let fixture: ComponentFixture<MediaComponent>;
 
   beforeEach(async(() => {
+    const mockActivatedRoute = {
+      snapshot: {
+        data: {}
+      }
+    };
+
     TestBed.configureTestingModule({
-      declarations: [ MediaComponent ]
+      declarations: [MediaComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: mockActivatedRoute
+        }
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
