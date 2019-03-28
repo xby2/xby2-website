ff80f92721a35650781f176cf3b30e4a5e6e6938
Dave Farinelli <d@dfar.io>
2019-03-28 14:13:04 -0400

    Adding HubSpot tracker

--- src/main.ts
+++ src/main.ts
@@ -11,14 +11,20 @@ if (environment.production) {
 if (environment.googleAnalyticsTrackingId !== '') {
   document.write(
     '<script>' +
-      '(function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||' +
-      'function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();' +
-      'a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;' +
-      'm.parentNode.insertBefore(a,m)})(window,document,\'script\',' +
-      '\'https://www.google-analytics.com/analytics.js\',\'ga\');' +
-      'ga(\'create\', \'' +
-      environment.googleAnalyticsTrackingId +
-      '\', \'auto\');</script>'
+    '(function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||' +
+    'function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();' +
+    'a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;' +
+    'm.parentNode.insertBefore(a,m)})(window,document,\'script\',' +
+    '\'https://www.google-analytics.com/analytics.js\',\'ga\');' +
+    'ga(\'create\', \'' +
+    environment.googleAnalyticsTrackingId +
+    '\', \'auto\');</script>'
+  );
+}
+
+if (environment.enableHubSpotTracking) {
+  document.write('<script type="text/javascript" id="hs-script-loader" async ' +
+    'defer src="//js.hs-scripts.com/5481705.js"></script>'
   );
 }
 
